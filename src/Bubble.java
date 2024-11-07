bf6b154ece9dcb74411f12189bf18ddc32a318a5
Pedro <no>
2024-11-07 11:04:53 -0300

    proficient level

--- src/Bubble.java
+++ src/Bubble.java
@@ -1,10 +1,38 @@
-/** THE ENTIRE CLASS IS INSIDE A COMMENT RIGHT NOW. TO ENABLE THE CLASS, DELETE THIS LINE AND THE LAST ONE.
+
 
 class Bubble {
 
-    //MISSING MANY INSTANCE VARIABLES
-
-    //MISSING CONSTRUCTORS
+    // instance variables that were previously mimssing, similar as ball.j
+    private Sketch s;
+    private float radius;
+    private float x;
+    private float y;
+    private float xSpeed;
+    private float ySpeed;
+    private int fillColor;
+    private int borderColor;
+    // constructor
+    public Bubble(Sketch sketch) {
+        s = sketch;
+        radius = 20;
+        x = 100;
+        y = 100;
+        xSpeed = 0;
+        ySpeed = -1;
+        fillColor = s.color(180, 180, 255);
+        borderColor = s.color(0, 0, 0);
+    }
+    // constructor overload 
+    public Bubble(Sketch sketch, float d, float x, float y, float xspeed){
+        this.s = sketch;
+        this.radius = d;
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xspeed;
+        this.ySpeed = -1;
+        fillColor = s.color(180, 180, 255);
+        borderColor = s.color(0, 0, 0);
+    }
 
     // accessors for the radius, diameter, x, and y values 
     public float getRadius() {
@@ -28,7 +56,7 @@ class Bubble {
         s.fill(fillColor);
         s.circle(x, y, radius*2);
     }
-
+    // calculates how and where the object moves
     public void move() {
         x = x + xSpeed;
         y = y + ySpeed;
@@ -44,4 +72,3 @@ class Bubble {
     }
 
 }
- **/
--- src/Snowflake.java
+++ src/Snowflake.java
@@ -1,9 +1,53 @@
-/** THE ENTIRE CLASS IS INSIDE A COMMENT RIGHT NOW. TO ENABLE THE CLASS, 
- * DELETE THESE LINE AND THE LAST ONE IN THE FILE.
 
 class Snowflake {
     // ONLY THE DRAW METHOD IS INCLUDED. YOU MUST FINISH THE REST.
-  
+    // instance variables that were previously mimssing, similar as ball.j
+    private Sketch s;
+    private float radius;
+    private float x;
+    private float y;
+    private float xSpeed;
+    private float ySpeed;
+    private int fillColor;
+    private int borderColor;
+     // constructor
+     public Snowflake(Sketch sketch) {
+        s = sketch;
+        radius = 15;
+        x = 50;
+        y = 500;
+        xSpeed = 0;
+        ySpeed = 1;
+        fillColor = s.color(180, 180, 255);
+        borderColor = s.color(0, 0, 0);
+     }
+     // constructor overload 
+    public Snowflake(Sketch sketch, float d, float x, float y, float xspeed){
+        this.s = sketch;
+        this.radius = d;
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xspeed;
+        this.ySpeed = 1;
+        fillColor = s.color(180, 180, 255);
+        borderColor = s.color(0, 0, 0);
+    }
+    // accessors for the radius, diameter, x, and y values 
+    public float getRadius() {
+        return radius;
+    }
+
+    public float getDiameter() {
+        return radius * 2;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
 
     public void draw() {
         s.line(x + radius, y, x - radius, y);
@@ -13,6 +57,17 @@ class Snowflake {
         s.line(x + radius * .707f, y - radius * .707f,
                x - radius * .707f, y + radius * .707f);
     }
+    public void move() { // copied from bubble.j since it was missing // calculates how and where the object moves
+        x = x + xSpeed;
+        y = y + ySpeed;
+        if (x > s.width - radius) {
+            x = radius;
+        } else if(x < radius) {
+            x = s.width - radius;
+        } else if (y > s.height - radius) {
+            y = radius;
+        } else if (y < radius) {
+            y = s.height - radius;
+        }
+    }
 }
-
-**/
